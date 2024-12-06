6661c9ff6a2d3a9f28580d580633328ab8976fec
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2024-12-06 17:11:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+function buscar(cep){
+    let ajax = new XMLHttpRequest();
+
+    ajax.open('GET', `https://viacep.com.br/ws/${cep}/json/`, true);
+
+    ajax.onload = function () {
+
+        if(ajax.status == 200){
+
+            loading.style.display = "none";
+
+            console.log('Resposta:', JSON.parse(ajax.responseText));
+
+            let endereco = JSON.parse(ajax.responseText);
+
+            let resultado = document.querySelector('.resultado');
+            resultado.innerHTML = "logradouro: " + endereco.logradouro + "<br>" + "bairro: " + endereco.bairro;
+
+        } else {
+            console.error('Erro:', ajax.status, ajax.statusText);
+        }
+    }
+
+    ajax.send(null);
+}
+
+
+let button = document.querySelector('button');
+
+let loading = document.querySelector('.carregando');
+
+button.addEventListener('click', ()=>{
+
+    loading.style.display = "block";
+
+    let input = document.querySelector('input');
+    let cep = input.value;
+
+    buscar(cep);
+
+});
